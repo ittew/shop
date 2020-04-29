7f10418be4130137d15818bac8b3971d47e41d5c
yangnan <yangnan@leadeon.cn>
2020-04-29 18:31:19 +0800

    fix:首页和tab样式优化

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -2,13 +2,18 @@
   <div class="index">
     <!-- 轮播图 -->
     <div class="swiper">
-      <swiper class="swiper-container" indicator-dots="true" autoplay="true" interval="3000" circular="true" duration="500">
-        <block v-for="(item, index) in banner " :key="index">
+      <swiper class="swiper-container" @change="swiperChange" autoplay="true" interval="3000" circular="true" duration="500">
+        <block v-for="(item, index) in banner " :key="index" >
           <swiper-item class="swiper-item">
             <image :src="item" class="slide-image" />
           </swiper-item>
         </block>
       </swiper>
+      <div class="dots">
+        <div v-for="(v,i) in banner" :key="i">
+          <div class="dotsItem" :class="i == swiperCurrent ? 'dotActive' : ''"></div>
+        </div>
+      </div>
     </div>
     <!-- icon -->
     <div class="icon grayLine">
@@ -21,12 +26,12 @@
     <div class="contentList grayLine" v-for="(v,i) in content" :key="i">
       <div class="barTitle">
         <div class="Title-left">{{v.title}}</div>
-        <div class="Title-right" @tap="gotoList">查看全部<span class="icon-right"></span></div>
+        <div class="Title-right" @tap="gotoList">查看全部<img src="/static/images/right-arrow.png" alt=""></div>
       </div>
       <div class="sublist">
         <div v-for="(subitem, subindex) in v.goodsList" :key="subindex" @click="jumpDetail">
           <img :src="subitem.url" alt="">
-          <p>{{subitem.name}}</p>
+          <p class="ellipsis-two">{{subitem.name}}</p>
           <p>￥{{subitem.retail_price}}</p>
         </div>
       </div>
@@ -38,6 +43,7 @@
 export default {
   data () {
     return {
+      swiperCurrent: 0,
       banner: [
         '/static/images/index/ad1.jpg',
         '/static/images/index/ad2.jpg',
@@ -48,35 +54,35 @@ export default {
       ],
       iconList: [
         {
-          url:'/static/images/user.png',
+          url:'/static/tabs/my-active.png',
           name: '超市'
         },
         {
-          url:'/static/images/user.png',
+          url:'/static/tabs/my-active.png',
           name: '超市'
         },
         {
-          url:'/static/images/user.png',
+          url:'/static/tabs/my-active.png',
           name: '超市'
         },
         {
-          url:'/static/images/user.png',
+          url:'/static/tabs/my-active.png',
           name: '超市'
         },
         {
-          url:'/static/images/user.png',
+          url:'/static/tabs/my-active.png',
           name: '超市'
         },
         {
-          url:'/static/images/user.png',
+          url:'/static/tabs/my-active.png',
           name: '超市'
         },
         {
-          url:'/static/images/user.png',
+          url:'/static/tabs/my-active.png',
           name: '超市'
         },
         {
-          url:'/static/images/user.png',
+          url:'/static/tabs/my-active.png',
           name: '超市'
         }
       ],
@@ -165,6 +171,9 @@ export default {
   components: {},
 
   methods: {
+    swiperChange(e) {
+      this.swiperCurrent = e.mp.detail.current
+    },
     gotoList() {},
     // 跳转到商品详情
     jumpDetail() {
@@ -185,6 +194,7 @@ export default {
   .swiper {
     width: 100%;
     height: 417rpx;
+    position: relative;
     .swiper-container {
       width: 100%;
       height: 100%;
@@ -200,44 +210,72 @@ export default {
       }
     }
   }
+  .dots{
+    width: auto;
+    height: 10rpx;
+    position: absolute;
+    bottom: 20rpx;
+    border-radius: 50rpx;
+    margin-left: 50%;
+    transform: translateX(-50%);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 3rpx 10rpx;
+  }
+  .dotsItem{
+    width: 10rpx;
+    height: 10rpx;
+    margin: 0rpx 8rpx;
+    border-radius: 50%;
+    background-color: rgba(255,255,255,.3);
+    transition: all .3s;
+  }
+  .dotActive{
+    width: 20rpx;
+    height: 10rpx;
+    border-radius: 40rpx;
+    background-color: #fff;
+  }
   .grayLine {
-    margin: 20rpx 0;
+    margin: 12rpx 0;
   }
   // icon
   .icon {
     display: flex;
     flex-wrap: wrap;
-    padding: 10rpx 0;
+    padding-bottom: 16rpx;
     background: #ffffff;
     div {
       width: 25%;
       text-align: center;
       img {
-        margin: 15rpx 0;
-        height: 100rpx;
-        width: 100rpx;
+        margin-top: 16rpx;
+        height: 92rpx;
+        width: 92rpx;
+        border: 1px solid #eee;
+        border-radius: 50%;
         display: inline-block;
       }
       p {
-        line-height: 1;
-        font-size: 30rpx;
-        margin-bottom: 15rpx;
+        font-size: 24rpx;
       }
     }
   }
   // 商品列表
   .contentList{
-    padding: 10rpx 20rpx 20rpx;
+    padding: 12rpx 20rpx 0;
     background: #ffffff;
   }
   .barTitle {
     width: 100%;
     height: 56rpx;
-    margin-top: 20rpx;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
     text-align: center;
   }
   .Title-left {
-    float: left;
     font-size: 37rpx;
     font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
     font-weight: 800;
@@ -245,29 +283,32 @@ export default {
   }
 
   .Title-right {
-    float: right;
     font-size: 26rpx;
     color: #aaa;
 
-  }
-  .icon-right {
-    // background: url("") no-repeat center center;
-    background-size: 64rpx 64rpx;
+    img {
+      width: 22rpx;
+      height: 22rpx;
+      margin-left: 6rpx;
+    }
   }
   // 商品样式
   .sublist {
     display: flex;
+    justify-content: space-between;
     flex-wrap: wrap;
+    padding: 8rpx 0;
     div {
-      width: 32%;
-      margin-right:1%;
+      width: 48%;
       box-sizing: border-box;
-      margin-top: 20rpx;
-      padding: 10px 0;
+      padding: 20rpx 0;
       background: #fff;
-      margin-bottom: 10rpx;
-      padding-bottom: 10rpx;
+      margin-bottom: 12rpx;
       border: 2rpx solid #eee;
+      border-radius: 14rpx;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
       img {
         display: block;
         width: 200rpx;
@@ -277,23 +318,21 @@ export default {
 
       p {
         margin-bottom: 5rpx;
-        text-indent: 1em;
-        font-size: 32rpx;
+        // text-indent: 1em;
+        font-size: 24rpx;
       }
 
       p:nth-child(2) {
-        overflow: hidden;
-        text-overflow: ellipsis;
-        white-space: nowrap;
-        width: 98%;
-        padding: 10px 0;
-        line-height: 1;
+        // width: 98%;
+        padding: 10rpx 20rpx 0;
       }
 
       p:nth-child(3) {
-        color: #9c3232;
+        padding: 10rpx 20rpx 0;
+        font-size: 30rpx;
+        font-weight: bold;
+        color: #f2270c;
       }
     }
-
-  }
+}
 </style>
