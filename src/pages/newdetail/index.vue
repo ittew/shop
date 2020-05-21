2a30920e959fd45db4dfac56a28abb08d781e261
马慧敏 <mahuimin@leadeon.cn>
2020-05-21 17:11:44 +0800

    feat:咨询详情

--- src/pages/newdetail/index.vue
+++ src/pages/newdetail/index.vue
@@ -9,75 +9,6 @@
 export default {
   data () {
     return {
-      sortList: [
-        {
-          name: '全部商品'
-        },
-        {
-          name: '移动办公'
-        },
-        {
-          name: '办公用品',
-          list: [{
-            name: '打印机',
-          },{
-            name: '复合机',
-          },{
-            name: '扫描仪',
-          },{
-            name: '复合机',
-          }],
-        },
-        {
-          name: '网络设备',
-          list: [{
-            name: '服务器CPU',
-          },{
-            name: '内存',
-          }],
-        },
-        {
-          name: '移动办公3'
-        },
-        {
-          name: '储存',
-          list: [{
-            name: '磁带',
-          },{
-            name: '超融合基础架构',
-          },{
-            name: '单控制器',
-          }],
-        },
-        {
-          name: '软件',
-          list: [{
-            name: '政府-党建云',
-          },{
-            name: '蓝凌-OA',
-          },{
-            name: '智能多端小程序',
-          },{
-            name: '智慧校园智能广告监测',
-          },{
-            name: '办公企业营销',
-          }],
-        },
-        {
-          name: '网络设备',
-          list: [{
-            name: '无线网卡',
-          },{
-            name: '有线网卡',
-          },{
-            name: '网络盒子',
-          },{
-            name: 'Android2',
-          },{
-            name: 'IOS1'
-          }],
-        }
-      ]
     }
   },
 
@@ -87,12 +18,6 @@ export default {
   },
 
   methods: {
-    // 跳转到商品详情
-    jumpDetail() {
-      wx.navigateTo({
-        url: '/pages/good/main'
-      })
-    }
   }
 }
 </script>
@@ -116,30 +41,6 @@ export default {
       font-size: 30rpx;
       text-indent: 2em;
       line-height: 40rpx;
-    }
-    .sort_li {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      text-align: center;
-      color: #999;
-      font-size: 30rpx;
-      border-bottom: 1rpx solid #efefef;
-      padding: 25rpx 30rpx 15rpx 30rpx;
-      .sorty {
-        color: #666;
-        font-size: 30rpx;
-      }
-    }
-    .sublist {
-      padding: 20rpx;
-      .sublist_li {
-        color:#999;
-        background-color: #f4f4f4;
-        padding: 14rpx;
-        margin: 8rpx;
-        display: inline-block;
-      }
     }
   }
 </style>
