22cedabd6356c7f9140e1838924b928f4032c0ad
yangnan <yangnan@leadeon.cn>
2020-06-08 17:38:11 +0800

    fix:地址列表接口联调

--- src/pages/address/index.vue
+++ src/pages/address/index.vue
@@ -7,18 +7,18 @@
           <div class="list" @touchstart="startMove" :data-index="index" @touchmove="deleteGoods" @touchend="endMove" v-for="(item, index) in listData" :key="index">
             <div class="addresslist" :style="item.textStyle">
                 <div>
-                  <span>{{item.name}}</span>
+                  <span>{{item.trueName}}</span>
                   <div v-if="item.is_default" class="moren">
                     默认
                   </div>
                 </div>
                 <div class="info">
                   <p>{{item.mobile}}</p>
-                  <p>{{item.address+item.address_detail}}</p>
+                  <p>{{item.area_info}}</p>
                 </div>
                 <div class="btns">
-                  <div @click="toDetail(item.id)" class="common edit"></div>
-                  <div @click="delAddress(item.id)" class="common delete"></div>
+                  <div @click="toDetail(item)" class="common edit"></div>
+                  <div @click="delAddress(item)" class="common delete"></div>
                 </div>
             </div>
 
@@ -36,52 +36,62 @@
 </template>
 
 <script>
-
+import { request } from "../../utils"
 export default {
   data() {
     return {
-      listData:[
-        {"id":1,"name": "gdsg","address": "天津市 天津市 河东区","mobile": "gdfgd","is_default": 1,"address_detail": "wefwewe"},
-        {"id":1,"name": "gdsg","address": "天津市 天津市 河东区","mobile": "gdfgd","is_default": 0,"address_detail": "wefwewe"},
-        {"id":1,"name": "gdsg","address": "天津市 天津市 河东区","mobile": "gdfgd","is_default": 0,"address_detail": "wefwewe"},
-        {"id":1,"name": "gdsg","address": "天津市 天津市 河东区","mobile": "gdfgd","is_default": 0,"address_detail": "wefwewe"},
-        {"id":1,"name": "gdsg","address": "天津市 天津市 河东区","mobile": "gdfgd","is_default": 0,"address_detail": "wefwewe"},
-        {"id":1,"name": "gdsg","address": "天津市 天津市 河东区","mobile": "gdfgd","is_default": 0,"address_detail": "wefwewe"},
-        {"id":1,"name": "gdsg","address": "天津市 天津市 河东区","mobile": "gdfgd","is_default": 0,"address_detail": "wefwewe"},
-        {"id":1,"name": "gdsg","address": "天津市 天津市 河东区","mobile": "gdfgd","is_default": 0,"address_detail": "wefwewe"},
-        {"id":1,"name": "gdsg","address": "天津市 天津市 河东区","mobile": "gdfgd","is_default": 0,"address_detail": "wefwewe"}
-      ]
+      listData: []
+      // listData:[
+      //   {"trueName": "gdsg","area_info": "天津市 天津市 河东区","mobile": "gdfgd","is_default": 1,"address_detail": "wefwewe"},
+      //   {"trueName": "gdsg","area_info": "天津市 天津市 河东区","mobile": "gdfgd","is_default": 0,"address_detail": "wefwewe"},
+      //   {"trueName": "gdsg","area_info": "天津市 天津市 河东区","mobile": "gdfgd","is_default": 0,"address_detail": "wefwewe"}
+      // ]
     }
   },
+  mounted() {
+    this.getData()
+  },
   methods: {
-    delAddress(id) {
-      var _this = this;
+    // 获取地址列表数据
+    async getData () {
+      console.log(222)
+      let data = {'store_id':'262158'} // 门店IDa
+      let url = 'card/address.htm'
+      let body = await request(url, 'get', data)
+      if (body.success) {
+        this.listData = body.data
+        console.log(body.data,'data')
+      }
+    },
+    delAddress(item) {
+      console.log(item)
+      let data = {'mulitId':item.id,'store_id':'22'} // 门店IDa
+      let url = 'card/address_del.htm'
       wx.showModal({
         title: "",
         content: "是否要删除该收货地址",
-        success: function(res) {
+        success: async function(res) {
           if (res.confirm) {
-             wx.showToast({
-              title: "删除成功",
-              icon: "success",
-              duration: 1500
-            });
-            // const data = get("/address/deleteAction", {
-            //   id: id
-            // }).then(() => {
-            //   _this.getAddressList();
-            // });
-          } else if (res.cancel) {
-            console.log("用户点击取消");
-            //滑动之前先初始化样式数据
-            _this.initTextStyle();
+            let body = await request(url, 'get', data)
+            console.log(body,'body')
+            if (body.success) {
+              wx.showToast({
+                title: "删除成功",
+                icon: "success",
+                duration: 1500
+              });
+              wx.navigateTo({
+                url: "/pages/address/main"
+              });
+            }
           }
         }
       });
     },
-    toDetail(id) {
+    toDetail(item) {
+      console.log(item)
       wx.navigateTo({
-        url: "/pages/addAddress/main"
+        url: "/pages/addAddress/main?id="+item.id
       });
     },
     getAddressList() {
@@ -131,7 +141,7 @@ export default {
     justify-content: space-between;
     align-items: center;
     div:nth-child(1) {
-      width: 100rpx;
+      width: 120rpx;
       text-align: center;
       overflow: hidden;
       text-overflow: ellipsis;
@@ -148,6 +158,7 @@ export default {
       }
     }
     .info {
+      flex:1;
       padding: 0 20rpx;
       // flex: 1; // p:nth-child(1) {}
       p:nth-child(2) {
